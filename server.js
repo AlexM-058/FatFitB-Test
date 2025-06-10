230d4843859328a9c962bd22c4ce9f2380624bfa
Alex <alexandrumateitarita@gmail.com>
2025-06-11 01:20:11 +0200

    Finish-Backend

--- server.js
+++ server.js
@@ -906,16 +906,6 @@ app.patch("/reset-password", async (req, res) => {
   }
 });
 
-// All API and JWT-protected routes above this line
-
-// Fallback route for SPA (serves index.html for any unmatched GET route, no auth)
-import path from "path";
-const __dirname = path.resolve();
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
